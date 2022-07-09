b4971da721b5174889fb2b638422340ef989ca6e
LuccasSilvaa <correaluccas@hotmail.com>
2022-07-09 18:19:23 -0300

    atividade ingles

---
+++ script.js
@@ -0,0 +1,63 @@
+let action = [
+  {
+    title: "Top Gun: Maverick",
+    director: "Joseph Kosinski",
+    year:2022,
+  },
+  {
+    title: "fast and the furious tokyo drift",
+    director: "Justin Lin",
+    year:2006,
+  },
+  {
+    title: "Mission: Impossible",
+    director: "Brian De Palma",
+    year:1996,
+  }
+]
+
+let comedy= [{
+    title:"The Wolf of Wall",
+    director: "Martin Scorsese",
+    year: 2014,
+  },
+  {
+    title:"White Chicks",
+    director: "Keenen Ivory Wayans",
+    year:  2004,
+  },
+  {
+    title:"Deadpool",
+    director: "Tim Miller",
+    year:  2016,
+  }
+]
+
+let fantasy =[
+  {
+    title:"Monster House",
+    director: "Gil Kenan",
+    year: 2006,
+  },
+  {
+    title:"Toy Story",
+    director: "John Lasseter",
+    year: 1995,
+  },
+  {
+    title:"Sonic 2",
+    director: "Jeff Fowler",
+    year: 2022,
+  }
+];
+
+
+
+//for(let action = 0; film <= action.length; action++){
+  //if(action[film] >= 2010){
+    //console.log(`film novo ${movie}`)
+  //}
+ //else{
+  //console.log(`filme antigo`)
+ //}
+//}
